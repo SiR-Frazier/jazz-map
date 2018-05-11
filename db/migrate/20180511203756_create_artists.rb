9eafa91955e1b50af5d0532b2b88caefa58d9d07
Bryce Frazier <bx.frazier@gmail.com>
2018-05-11 13:53:18 -0700

    figuring out how to structure discography array for db

--- db/migrate/20180511203756_create_artists.rb
+++ db/migrate/20180511203756_create_artists.rb
@@ -1,6 +1,10 @@
 class CreateArtists < ActiveRecord::Migration[5.2]
   def change
     create_table :artists do |t|
+      t.column :name, :string
+      t.column :artist_site, :string
+      t.column :biography, :varchar
+      t.column :discography, :varchar, array:true, default: []
     end
   end
 end
